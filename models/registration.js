ede6e7a9266f7ae6373d7d1ef7b592823231f265
sree vishnu <9539495864sree@gmail.com>
2023-12-02 21:38:59 +0530

    technova registration schema is added.

---
+++ models/registration.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const technovaRegistrationSchema = mongoose.Schema({
+    collegename: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    participants: [
+        {
+            username: {
+                type: String,
+                required: true
+            },
+            phone: {
+                type: String,
+                required: true
+            },
+            isAdmin: {
+                type: Boolean,
+                default: false
+            }
+        }
+    ],
+    paymentStatus: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const TechnovaRegistration = mongoose.model('technovaregistration', technovaRegistrationSchema);
+
+export default TechnovaRegistration;
