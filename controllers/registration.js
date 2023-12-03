9c2bc3966ed93f0464c1d2f19d3e1e56c1fe1aee
sree vishnu <9539495864sree@gmail.com>
2023-12-03 14:06:04 +0530

    registration controller is completed.

--- controllers/registration.js
+++ controllers/registration.js
@@ -1,8 +1,21 @@
+import TechnovaRegistration from "../models/registration.js";
+
 export const technovaRegistration = async (req, res) => {
     try {
         const Data = req.body;
-        
+
+        const existingRegistration = await TechnovaRegistration.findOne({ email: req.body.email });
+
+        if (existingRegistration) return res.status(400).json({ message: "Email is already registred." });
+
+        const savedData = await TechnovaRegistration.create(Data);
+
+        if (!savedData) return res.status(400).json({ message: "Registration is failed due to server error." })
+
+        res.status(200).json({ message: "You are successfully registered." });
+
     } catch (error) {
         console.log(error);
+        req.status(500).json({ message: `Registration failed due to internal server error : ${error.response}` });
     }
 }
