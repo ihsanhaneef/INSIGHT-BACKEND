16202a986afcd98f3ebd387c1c60792bdb371022
ihsanhaneef <ihsanhaneef@gmail.com>
2023-12-04 13:02:42 +0530

    initial commit

--- controllers/registration.js
+++ controllers/registration.js
@@ -2,20 +2,22 @@ import TechnovaRegistration from "../models/registration.js";
 
 export const technovaRegistration = async (req, res) => {
     try {
-        const Data = req.body;
+        const { email, collegename, participants } = req.body;
+
+        if (!email || !collegename || !participants) return res.status(400).json({ message: "Missing of essential data from request body." });
 
         const existingRegistration = await TechnovaRegistration.findOne({ email: req.body.email });
 
         if (existingRegistration) return res.status(400).json({ message: "Email is already registred." });
 
-        const savedData = await TechnovaRegistration.create(Data);
-
+        const savedData = await TechnovaRegistration.create(req.body);
+        
         if (!savedData) return res.status(400).json({ message: "Registration is failed due to server error." })
-
+    
         res.status(200).json({ message: "You are successfully registered." });
-
+        
     } catch (error) {
         console.log(error);
-        req.status(500).json({ message: `Registration failed due to internal server error : ${error.response}` });
+        res.status(500).json({ message: `Registration failed due to internal server error : ${error.response}` });
     }
 }
--- index.js
+++ index.js
@@ -9,7 +9,15 @@ const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
-app.use(cors());
+app.use(cors({
+    origin:"*"
+}));
+
+// const corsOptions = {
+//     origin: 'http://localhost:5501',
+// };
+
+// app.use(cors(corsOptions));
 
 app.use('/api/technova', technovaRegistrationRouter);
 
